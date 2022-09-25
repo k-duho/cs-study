457144005cfef194d20714c75e607e80552447c2
duho Kim <dev.kimduho@gmail.com>
2022-09-25 17:12:44 +0900

    [ruby]stack - refactoring

--- stack.rb
+++ stack.rb
@@ -4,15 +4,26 @@ class Stack
   def initialize
     @list = []
     @last_index = -1
+
   end
 
   def push(object)
-    list << object
+    @list << object
     @last_index += 1
   end
 
   def pop
-    list[last_index]
     @last_index -= 1
+    @list.shift
   end
 end
+
+
+stack = Stack.new
+stack.push 1
+stack.push 12
+stack.push 13
+p stack.list
+p stack.pop
+
+
