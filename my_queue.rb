f970c4ed72206e7887783ce6b81934ed848d0fb0
duho Kim <dev.kimduho@gmail.com>
2022-09-25 21:50:58 +0900

    [ruby]queue

---
+++ my_queue.rb
@@ -0,0 +1,35 @@
+class MyQueue
+  attr_reader :list, :last_index
+
+  def initialize
+    @list = []
+    @last_index = -1
+  end
+
+  def push(object)
+    @list << object
+    @last_index += 1
+  end
+
+  def pop
+    @last_index -= 1
+    @list.pop
+  end
+
+  def first
+    @list.first
+  end
+
+  def empty?
+    @list.empty?
+  end
+end
+
+queue = MyQueue.new
+queue.push 1
+queue.push 12
+queue.push 13
+p queue.list
+p queue.pop
+
+
